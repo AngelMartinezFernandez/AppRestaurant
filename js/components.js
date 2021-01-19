a6ce232b72a4c599225174ce5b9c6a3e2c624c34
AngelMartinezFernandez <angelmanuelmartinezfernandez@gmail.com>
2021-01-19 10:14:01 +0100

    Se soluciona problema con footer

--- js/components.js
+++ js/components.js
@@ -1,4 +1,32 @@
+const mainSelector = document.querySelector("#productos");
 
+export const insertarProductosHtml = (platos)=>{
+
+    for (const plato of platos) {
+        
+    const section = document.createElement('section');
+    section.innerHTML= `<section>
+    <article class="menu-plato">
+        <figure>
+            <img class="img-producto" src="${plato.foto}" alt="">
+        </figure> 
+        <div class="descplato"> 
+                <p class="precio">${plato.precio}€</p>
+                <p class="tituloproducto">${plato.nombre}</p>
+                <p class="descproducto">${plato.descripcion}</p>
+            <ul class="contador">
+                <li><button id="btnmn-id${plato.id}" class="btn-menos btn-mm">-</button></li>
+                <li><label id="counter-id${plato.id}" class="btn-counter" for="">00</label></li>
+                <li><button id="btnms-id${plato.id}" class="btn-mas btn-mm">+</button></li>
+            </ul>
+        </div> 
+    
+    </article>
+    </section>`;
+
+    mainSelector.append(section.firstElementChild);
+
+}};
 
 
 
--- js/productos.js
+++ js/productos.js
@@ -1,20 +1,9 @@
-
-
-
-
-
-//Selectores
-const mainSelector = document.querySelector("#productos");
-
-
-//Arrays Productos
-const platos = [
+export let platos = [
     
     {
         id: 1,
         nombre: "Fabada Asturiana",
         precio: 13.90,
-        unidades: 1,
         descripcion:"Tabla de Fabada Asturiana para 3 personas.",
         foto:"./img/productos/platos/fabada.png",
         categoria: "platos"
@@ -23,7 +12,6 @@ const platos = [
         id: 2,
         nombre: "Licor de Guindas Asturiano",
         precio: 13.75,
-        unidades: 0,
         descripcion:"Botella de licor asturiano 70cl.",
         foto:"./img/productos/bebidas/licordeguindas.png",
         categoria: "bebidas"
@@ -32,7 +20,6 @@ const platos = [
         id: 3,
         nombre: "Casadiellas Asturianas",
         precio: 4.95,
-        unidades: 0,
         descripcion:"Media docena de Casadiellas.",
         foto:"./img/productos/postres/casadiellas.png",
         categoria: "postres"
@@ -41,7 +28,6 @@ const platos = [
         id: 4,
         nombre: "Licor de Guindas Asturiano",
         precio: 13.75,
-        unidades: 0,
         descripcion:"Botella de licor asturiano 70cl.",
         foto:"./img/productos/bebidas/licordeguindas.png",
         categoria: "bebidas"
@@ -50,7 +36,6 @@ const platos = [
         id: 5,
         nombre: "Casadiellas Asturianas",
         precio: 4.95,
-        unidades: 0,
         descripcion:"Media docena de Casadiellas.",
         foto:"../img/productos/postres/casadiellas.png",
         categoria: "postres"
@@ -59,7 +44,6 @@ const platos = [
         id: 6,
         nombre: "Fabada Asturiana",
         precio: 13.90,
-        unidades: 0,
         descripcion:"Tabla de Fabada Asturiana para 3 personas.",
         foto:"./img/productos/platos/fabada.png",
         categoria: "platos"
@@ -68,7 +52,6 @@ const platos = [
         id: 7,
         nombre: "Licor de Guindas Asturiano",
         precio: 13.75,
-        unidades: 0,
         descripcion:"Botella de licor asturiano 70cl.",
         foto:"./img/productos/bebidas/licordeguindas.png",
         categoria: "bebidas"
@@ -77,73 +60,9 @@ const platos = [
         id: 8,
         nombre: "Casadiellas Asturianas",
         precio: 4.95,
-        unidades: 0,
         descripcion:"Media docena de Casadiellas.",
         foto:"./img/productos/postres/casadiellas.png",
         categoria: "postres"
     },
     
 ];
-
-let carrito = [];
-let total = 0;
-
-
-//Inyección HTML
-
-const insertarProductosHtml = (platos)=>{
-
-    for (const plato of platos) {
-
-        const section = document.createElement('section');
-
-        //template
-        
-        section.innerHTML= `<section>
-        <article class="menu-plato" id="${plato.id}">
-            <figure>
-                <img class="img-producto" src="${plato.foto}" alt="">
-            </figure> 
-            <div class="descplato"> 
-                    <p class="precio">${plato.precio}€</p>
-                    <p class="tituloproducto">${plato.nombre}</p>
-                    <p class="descproducto">${plato.descripcion}</p>
-                <ul class="contador">
-                    <li><button class="btn-menos btn-mm">-</button></li>
-                    <li><label class="btn-counter" counter="${plato.unidades}">${plato.unidades}</label></li>
-                    <li><button class="btn-mas btn-mm">+</button></li>
-                </ul>
-            </div> 
-        
-        </article>
-        </section>`;
-
-    mainSelector.append(section.firstElementChild);
-
-}};
-
-const getPlato= (id)=>{
-
-    //pasandole un elemento recorrer array y encontrar el elemento
-    //
-return objeto;
-}
-
-const sumarPlato= ()=>{
-
-    
-    carrito.push(getPlato(id));
-    
-
-    //modificar tambien cantidad.
-
-}
-
-
-
-
-
-
-
-
-insertarProductosHtml(platos);
